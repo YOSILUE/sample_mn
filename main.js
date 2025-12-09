27cf1648867bca0dfb24b1cc5187abfbcb5ee849
YOSILUE <44741027+YOSILUE@users.noreply.github.com>
2025-12-10 08:33:22 +0900

    Remove trailing slash from modelPath URL

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ log("[INIT] wasmPaths = " + ort.env.wasm.wasmPaths);
 //----------------------------------------------------
 // モデルロード
 //----------------------------------------------------
-const modelPath = "https://yosilue.github.io/sample_mn/model/best.onnx/";
+const modelPath = "https://yosilue.github.io/sample_mn/model/best.onnx";
 log("[INIT] モデルパス = " + modelPath);
 
 // グローバル変数
