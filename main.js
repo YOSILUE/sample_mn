bfb8430e0e0c3bd97ddbd744feb63ef75cef7e6d
YOSILUE <44741027+YOSILUE@users.noreply.github.com>
2025-12-11 09:22:37 +0900

    Update model path to best_04.onnx

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ log("[INIT] wasmPaths = " + ort.env.wasm.wasmPaths);
 //----------------------------------------------------
 // モデルロード
 //----------------------------------------------------
-const modelPath = "https://yosilue.github.io/sample_mn/model/best_03.onnx";
+const modelPath = "https://yosilue.github.io/sample_mn/model/best_04.onnx";
 log("[INIT] モデルパス = " + modelPath);
 
 // グローバル変数
