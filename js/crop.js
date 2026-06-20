c5a6a795c9eb276dd8b021886ce9f876c080af79
YOSILUE <44741027+YOSILUE@users.noreply.github.com>
2026-06-20 23:40:26 +0900

    Refactor cropBox function and add logging
    Refactor cropBox function to improve calculations and add logging.

--- js/crop.js
+++ js/crop.js
@@ -1,22 +1,29 @@
-export function cropBoxFromImage(imgElement, box) {
+import { log } from "https://yosilue.github.io/sample_mn/js/logger.js";
+
+//----------------------------------------------------
+// BOX領域切り出し
+//----------------------------------------------------
+export function cropBox(imgElement, box) {
 
   const imgW = imgElement.naturalWidth;
   const imgH = imgElement.naturalHeight;
 
-  // 推論640 → 元画像スケール
+  // 推論空間(640) → 元画像空間
   const sx = imgW / 640;
   const sy = imgH / 640;
 
-  const x = Math.max(0, box.x1 * sx);
-  const y = Math.max(0, box.y1 * sy);
-  const w = Math.min(imgW, (box.x2 - box.x1) * sx);
-  const h = Math.min(imgH, (box.y2 - box.y1) * sy);
+  const x = Math.max(0, Math.floor(box.x1 * sx));
+  const y = Math.max(0, Math.floor(box.y1 * sy));
+  const w = Math.max( 1, Math.floor((box.x2 - box.x1) * sx));
+  const h = Math.max( 1, Math.floor((box.y2 - box.y1) * sy));
+
+  log(`[CROP] x=${x}, y=${y}, w=${w}, h=${h}`);
 
   const canvas = document.createElement("canvas");
   const ctx = canvas.getContext("2d");
 
-  canvas.width = Math.max(1, Math.floor(w));
-  canvas.height = Math.max(1, Math.floor(h));
+  canvas.width = w;
+  canvas.height = h;
 
   ctx.drawImage(
     imgElement,
@@ -26,8 +33,8 @@ export function cropBoxFromImage(imgElement, box) {
     h,
     0,
     0,
-    canvas.width,
-    canvas.height
+    w,
+    h
   );
 
   return canvas;
